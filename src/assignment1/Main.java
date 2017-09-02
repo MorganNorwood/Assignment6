24bcce63de9f466ebf0c5b2c68f5a8f54a0fc06d
Morgan Norwood <morgannorwood@gmail.com>
2017-09-02 15:49:44 -0400

    Assignment 1

---
+++ src/assignment1/Main.java
@@ -0,0 +1,15 @@
+/* Morgan Norwood
+Assignment 1
+*/
+package assignment1;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //welcome to java will print 3 times.
+	System.out.println("Welcome to Java!");
+	System.out.println("Welcome to Java!");
+	System.out.println("Welcome to Java!");
+
+    }
+}
