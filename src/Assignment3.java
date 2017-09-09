5cd6cc07d4c5f7044833d99b598fc5be2936801f
Morgan Norwood <morgannorwood@gmail.com>
2017-09-09 15:41:25 -0400

    Assignment 3

---
+++ src/Assignment3.java
@@ -0,0 +1,40 @@
+/*
+
+Morgan Norwood
+Assignment 3.14 Heads or Tails.
+9/7/2017
+
+ */
+import junit.framework.JUnit4TestAdapter;
+import org.junit.runners.JUnit4;
+
+import java.util.Scanner;
+
+public class Assignment3 {
+
+    public static void main(String[] args) {
+
+        int num;
+        int guess;
+
+        num = (int)(Math.random() *2);
+        //making a random int
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Heads or Tails? (enter 0 for heads and 1 for tails) ");
+        guess = input.nextInt();
+
+        //code will determine if the number entered is the same as the random number (between 0-1) are the same.
+        if (guess == num) {
+            System.out.println("You win");
+        }else{
+            System.out.println("you lose");
+        }
+
+
+
+
+
+
+    }
+}
